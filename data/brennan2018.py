b1377a32ec7866e76bd9e768b4937e30fc6997d7
RomanKoshkin <roman.koshkin@gmail.com>
2022-11-26 16:41:38 +0900

    more experiments with accuracy calc, simplified Breannan (sim. to Gwill.), bsz-agnostic CLIPLoss

--- data/brennan2018.py
+++ data/brennan2018.py
@@ -85,10 +85,11 @@ class Brennan2018Dataset(torch.utils.data.Dataset):
                 mode=mode,
             ), Y_path)
 
-        self.Y = torch.load(Y_path)  # load the upsampled (to 120 Hz) embeddings (of the entire recording)
+        # load the upsampled (to 120 Hz) embeddings (of the entire recording)
+        self.Y = torch.load(Y_path)
 
+        # load or rebuild the array of pre-processed EEG. Shape: (subj, chan, time)
         X_path = "data/Brennan2018/processed_X.pt"
-
         if (not os.path.exists(X_path)) or force_recompute[0]:
             cprint(f'Pre-processing EEG...', color='red')
             self.X, srate = self.brain_preproc(audio_embd_len=self.Y.shape[-1])
@@ -103,9 +104,12 @@ class Brennan2018Dataset(torch.utils.data.Dataset):
         srate = preprocessed_eeg['srate']  # ( 33, 60, 99712 )
         cprint(f"Using existing pre-processed data {self.X.shape}, srate={srate}", 'red', 'on_yellow')
 
+        self.num_subjects = self.X.shape[0]
+        cprint(f'Number of subjects: {self.num_subjects}', color='yellow')
+
         self.X, self.Y = self.shift_brain_signal(self.X, self.Y, srate=srate)
 
-        cprint(f"X: {self.X.shape}, Y: {self.Y.shape}", color='red', attrs=['bold'])
+        cprint(f"X (EEG): {self.X.shape}, Y (audio embeds): {self.Y.shape}", color='red', attrs=['bold'])
         # X: ( 33, 60, 86791 ) -> ( B, 60, 1024 )
         # Y: ( 1024, 86791 ) -> ( B, 1024, 1024 ) # w2v embeddings
 
@@ -115,27 +119,37 @@ class Brennan2018Dataset(torch.utils.data.Dataset):
         # length of baseline period in samples
         self.baseline_len_samp = int(self.seq_len_samp * self.baseline_len_sec / self.seq_len_sec)
 
-        cprint(f'Building batches of {self.seq_len_sec} s ({self.seq_len_samp} samples).', color='blue')
-        if train:
-            self.Y = self.Y[..., :int(self.Y.shape[-1] * 0.7)]
-            self.X = self.X[..., :int(self.X.shape[-1] * 0.7)]
-        else:
-            self.Y = self.Y[..., int(self.Y.shape[-1] * 0.7):]
-            self.X = self.X[..., int(self.X.shape[-1] * 0.7):]
-        self.X, self.Y, self.subject_idxs, self.chunk_ids = self.batchfy(self.X, self.Y)
-        cprint(f'X: {self.X.shape} | Y: {self.Y.shape} | {self.subject_idxs.shape}', color='blue')
+        # compute the number if samples that divide evenly by the number of samples in 1 segement
+        trim_len = (self.X.shape[-1] // self.seq_len_samp) * self.seq_len_samp
+
+        # compute the number of segements in the entire dataset
+        num_segments = trim_len // self.seq_len_samp
+
+        # trim the length of EEG and embeddings so that they can be evenly divided by num time samp in 1 segment
+        self.X = self.X[..., :trim_len]
+        self.Y = self.Y[..., :trim_len]
+
+        # make segments
+        self.X = self.X.split(num_segments, dim=-1)
+        self.Y = self.Y.split(num_segments, dim=-1)
+
+        # self.X, self.Y, self.subject_idxs, self.chunk_ids = self.batchfy(self.X, self.Y)
+        # cprint(f'X: {self.X.shape} | Y: {self.Y.shape} | {self.subject_idxs.shape}', color='blue')
 
     def __len__(self):
         return len(self.X)
 
     def __getitem__(self, i, return_chunkids=True):
+        random_subject = np.random.choice(self.num_subjects)
         if return_chunkids:
-            return self.X[i], self.Y[i], self.subject_idxs[i], self.chunk_ids[i]
+            return self.X[i][random_subject], self.Y[i], random_subject, i
         else:
-            return self.X[i], self.Y[i], self.subject_idxs[i]
+            return self.X[i][random_subject], self.Y[i], random_subject
 
     def batchfy(self, X: torch.Tensor, Y: torch.Tensor):
-        # NOTE: self.seq_len_samp is `bptt`
+        """"
+        legacy
+        """
 
         assert X.shape[-1] == Y.shape[-1]
         trim_len = X.shape[-1] - X.shape[-1] % self.seq_len_samp
@@ -282,3 +296,10 @@ class Brennan2018Dataset(torch.utils.data.Dataset):
         Y = Y[:, :-shift]  # ( 512, 99692 )
 
         return X, Y
+
+
+if __name__ == "__main__":
+    from configs.args import args
+    ds = Brennan2018Dataset(args, train=True)
+    print(0)
+    print(0)
--- utils/get_dataloaders.py
+++ utils/get_dataloaders.py
@@ -2,52 +2,45 @@ from torch.utils.data import DataLoader, RandomSampler, BatchSampler
 from data.brennan2018 import CustomBatchSampler
 
 
-def get_dataloaders(train_set, test_set, args, g, seed_worker):
+def get_dataloaders(train_set, test_set, args, g, seed_worker, test_bsz=None):
 
-    if args.use_sampler:
-        train_batch_sampler = CustomBatchSampler(train_set, args)
-        test_batch_sampler = CustomBatchSampler(test_set, args)
-    else:
-        train_batch_sampler = None
-        test_batch_sampler = None
-
-    if args.use_sampler:
+    if args.reproducible:
         train_loader = DataLoader(
             train_set,
-            shuffle=False,
-            batch_sampler=train_batch_sampler,
-            num_workers=6,
-            pin_memory=True,
-            worker_init_fn=seed_worker,
-            generator=g,
-        )
-        test_loader = DataLoader(
-            test_set,
-            shuffle=False,
-            batch_sampler=test_batch_sampler,
-            num_workers=6,
-            pin_memory=True,
-            worker_init_fn=seed_worker,
-            generator=g,
-        )
-    else:
-        train_loader = DataLoader(
-            train_set,
+            batch_size=args.batch_size,
+            drop_last=True,
             shuffle=True,
-            batch_size=args.batch_size,
             num_workers=6,
-            pin_memory=False,
+            pin_memory=True,
             worker_init_fn=seed_worker,
             generator=g,
         )
         test_loader = DataLoader(
             test_set,
+            batch_size=args.batch_size if test_bsz is not None else test_bsz,
+            drop_last=True,
             shuffle=False,
-            batch_size=args.batch_size,
             num_workers=6,
-            pin_memory=False,
+            pin_memory=True,
             worker_init_fn=seed_worker,
             generator=g,
+        )
+    else:
+        train_loader = DataLoader(
+            train_set,
+            batch_size=args.batch_size,
+            drop_last=False,
+            shuffle=True,
+            num_workers=6,
+            pin_memory=True,
+        )
+        test_loader = DataLoader(
+            test_set,
+            batch_size=args.batch_size if test_bsz is not None else test_bsz,
+            drop_last=True,
+            shuffle=False,
+            num_workers=6,
+            pin_memory=True,
         )
 
     return train_loader, test_loader
