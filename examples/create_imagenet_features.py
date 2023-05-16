78d603a8eb495c2e991beaf4b14a741fa33f6da5
inoue26 <inoue@araya.org>
2023-05-16 15:29:28 +0900

    sbj01/02/03 is trained in single sub way

--- examples/create_imagenet_features.py
+++ examples/create_imagenet_features.py
@@ -17,8 +17,10 @@ model, preprocess = clip.load("ViT-B/32", device=device)
 model = model.eval()
 
 # images_dir = "./imagenet_fall2011_oneImagePerCat_21-2k_20230309/"
-images_dir = "/storage/dataset/image/ImageNet/ILSVRC2012_val/"
+images_dir = "/storage/dataset/image/ImageNet/ILSVRC2012_train/"
+print('start getting image file name list')
 list_images = os.listdir(images_dir)
+print('end getting image file name list')
 len(list_images)
 
 image_names_list = []
@@ -47,6 +49,6 @@ name_feat_dict = {}
 for img_name, feat_val in zip(image_names_list, image_feats_list):
     name_feat_dict[img_name] = np.squeeze(feat_val)
 
-a_file = open("/home/yainoue/meg2image/codes/MEG-decoding/data/ImageNet/val_features.pkl", "wb")
+a_file = open("/home/yainoue/meg2image/codes/MEG-decoding/data/ImageNet/train_features.pkl", "wb")
 pickle.dump(name_feat_dict, a_file)
 a_file.close()
